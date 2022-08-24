f1fb55d0d40d03a75629637af1bfce691a0814d2
Mayara Silva <devmayara11@gmail.com>
2022-08-24 17:44:20 -0300

    Incluindo Imagens e Ícones no Template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -16,7 +16,7 @@
 <body>
     <div class="container">
         <div class="sidebar">
-            Logo
+            <img src="/assets/images/logo.png">
         </div>
         <div class="content">
             <nav>
@@ -28,12 +28,15 @@
                 <section class="graph">
                     <div class="graph_header">
                         <h2>Progresso do Dia</h2>
-                        <hr class="linha_header">
-                        Data
+                        <div class="graph_header_line"></div>
+                        <div class="graph_header_date">Data</div>
                     </div>
                     <div class="graph_header-subtitle"> Tarefas: <b>3/6</b> </div>
                     <div class="graph-placeholder"></div>
-                    <p class="graph_header-tasks_left"> Restam 3 tarefas para serem realizadas </p>
+                    <div class="tasks_left_footer">
+                        <img src="/assets/images/icon-info.png">
+                         Restam 3 tarefas para serem realizadas
+                    </div>
                 </section>
                 <section class="list">
                     <div class="list_header">
@@ -52,7 +55,12 @@
                                 <div> tarefa</div>
                             </div>
                             <div class="actions">
-                                Editar - Excluir
+                                <a href="#">
+                                    <img src="/assets/images/icon-edit.png">
+                                </a>
+                                <a href="#">
+                                    <img src="/assets/images/icon-delete.png">
+                                </a>
                             </div>
                         </div>
                         <div class="task">
@@ -65,7 +73,12 @@
                                 <div> tarefa</div>
                             </div>
                             <div class="actions">
-                                Editar - Excluir
+                                <a href="#">
+                                    <img src="/assets/images/icon-edit.png">
+                                </a>
+                                <a href="#">
+                                    <img src="/assets/images/icon-delete.png">
+                                </a>
                             </div>
                         </div>
                     </div>
