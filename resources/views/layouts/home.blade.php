7d8a621a17352240fb49c482303c66d945a3ffdc
Mayara Silva <devmayara11@gmail.com>
2022-08-24 17:13:34 -0300

    Finalizando a parte Estrutural do Template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -48,7 +48,20 @@
                                 <div class="task_title"> Título da tarefa </div>
                             </div>
                             <div class="priority">
-                                <div class="sphere"> </div>
+                                <div class="sphere"></div>
+                                <div> tarefa</div>
+                            </div>
+                            <div class="actions">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox">
+                                <div class="task_title"> Título da tarefa </div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
                                 <div> tarefa</div>
                             </div>
                             <div class="actions">
